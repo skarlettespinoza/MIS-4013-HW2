3babf7ab9077284f5b159eb243f7e38dc73817e2
skarlettespinoza <123094235+skarlettespinoza@users.noreply.github.com>
2024-09-12 15:24:54 -0500

    Create post.php

---
+++ post.php
@@ -0,0 +1,12 @@
+<?php
+$pageTitle="Post";
+include "view-header.php";
+?>
+<h1>Post</h1>
+<form method="post" action"post-result.php">
+  <input type="text" name="my-name">
+  <input type="submit" value"Post data">
+</form>  
+<?php
+include "view-footer.php";
+?>
