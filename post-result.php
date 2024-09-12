4df7be1391d89c293973da5c5d1b652520bb273d
skarlettespinoza <123094235+skarlettespinoza@users.noreply.github.com>
2024-09-12 16:00:58 -0500

    Update post-result.php

--- post-result.php
+++ post-result.php
@@ -4,6 +4,15 @@ include "view-header.php";
 ?>
 <h1>Post result</h1>
 <?php
-if (isset($_POST['my-name']))
+if (isset($_POST['my-name'])) {
+?>
+  <p>The value sent is:</p>
+<?php
+  echo$_POST['my-name'];
+} else {
+?>
+  <p>Nothing posted to the page.</p>
+<?php 
+}
 include "view-footer.php";
 ?>
