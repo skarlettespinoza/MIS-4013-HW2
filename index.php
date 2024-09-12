7bd8c16f8ddbad95210e522f4f50d82899a9c260
skarlettespinoza <123094235+skarlettespinoza@users.noreply.github.com>
2024-09-12 11:09:23 -0500

    Update index.php

--- index.php
+++ index.php
@@ -8,6 +8,33 @@
   </head>
   <body>
     <div class="container>
+      <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="post.php">Post</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="post-result.php">Post result</a>
+        </li>
+         <li class="nav-item">
+          <a class="nav-link" href="get.php">Get</a>
+        </li>
+         <li class="nav-item">
+          <a class="nav-link" href="get-result.php">Get result</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
     <h1>Hello, world!</h1>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
