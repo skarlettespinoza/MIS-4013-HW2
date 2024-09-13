c2af8c8865ebf098d0823e09242e0fdc2641dcc5
skarlettespinoza <123094235+skarlettespinoza@users.noreply.github.com>
2024-09-12 22:17:44 -0500

    Update post-result.php

--- post-result.php
+++ post-result.php
@@ -4,15 +4,14 @@ include "view-header.php";
 ?>
 <h1>Post result</h1>
 <?php
-if (isset($_POST['my-name'])) {
-?>
-  <p>The value sent is:</p>
-<?php
-  echo $_POST['my-name'];
-} else {
-?>
-  <p>Nothing posted to the page.</p>
-<?php 
-}
+echo getDisplay();
 include "view-footer.php";
+
+function getDisplay() {
+  if (isset($_POST['my-name'])) {
+    return"<p>The value sent is:</p>" . echo $_POST['my-name'];
+} else {
+  return "<p>Nothing posted to the page.</p>";
+}
+}
 ?>
