15f7f4468539ca29bb6f3870b5acc84e552e47b0
skarlettespinoza <123094235+skarlettespinoza@users.noreply.github.com>
2024-09-12 22:33:37 -0500

    Update get.php

--- get.php
+++ get.php
@@ -3,7 +3,7 @@ $pageTitle="Get";
 include "view-header.php";
 ?>
 <h1>Get</h1>
-<form method="get" action"get-result.php">
+<form method="get" action="get-result.php">
   <input type="text" name="my-name">
   <input type="submit" value"Get data">
 </form>  
