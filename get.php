efdd8144efb7bbb67f6f8ebafb44a37e39b90564
skarlettespinoza <123094235+skarlettespinoza@users.noreply.github.com>
2024-09-12 22:08:19 -0500

    Create get.php

---
+++ get.php
@@ -0,0 +1,12 @@
+<?php
+$pageTitle="Get";
+include "view-header.php";
+?>
+<h1>Post</h1>
+<form method="get" action"get-result.php">
+  <input type="text" name="my-name">
+  <input type="submit" value"Get data">
+</form>  
+<?php
+include "view-footer.php";
+?>
