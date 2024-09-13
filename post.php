58cf61326c6470f688db5428f3ad6f5fa5e68391
skarlettespinoza <123094235+skarlettespinoza@users.noreply.github.com>
2024-09-12 22:37:16 -0500

    Update post.php

--- post.php
+++ post.php
@@ -5,7 +5,7 @@ include "view-header.php";
 <h1>Post</h1>
 <form method="post" action="post-result.php">
   <input type="text" name="my-name">
-  <input type="submit" value"Post data">
+  <input type="submit" value="Post data">
 </form>  
 <?php
 include "view-footer.php";
